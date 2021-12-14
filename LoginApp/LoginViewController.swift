3769f234e5ac7d9afb063d4e7c904108d3a298f1
Aleksandr Khasanov <sashokhasanov@mail.ru>
2021-12-14 23:57:30 +0300

    - implemented alerts - implementes segues - implemented welcome view controller

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    private let login = "Happy"
+    private let password = "123"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "loginSegue") {
+            let welocmeController = segue.destination as! WelcomeViewController
+            welocmeController.login = login
+        }
     }
-
-
+    
+    @IBAction func loginButtonPressed() {
+        if (checkLoginAndPassword()) {
+            performSegue(withIdentifier: "loginSegue", sender: self)
+        } else {
+            showAlert(title: "Oops 😥", message: "invalid login or password")
+        }
+    }
+    
+    @IBAction func unwindLoginSegue(_ segue: UIStoryboardSegue) {
+        loginTextField.text = nil
+        passwordTextField.text = nil
+    }
+    
+    @IBAction func forgotLoginpressed() {
+        showAlert(title: "🦄", message: "username is \(login)")
+    }
+    
+    @IBAction func forgotPasswordPressed() {
+        showAlert(title: "🦄", message: "password is \(password)")
+    }
+    
+    private func checkLoginAndPassword() -> Bool {
+        let inputLogin = loginTextField.text ?? ""
+        let inputPassword = passwordTextField.text ?? ""
+        
+        return login == inputLogin && password == inputPassword
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 }
 
